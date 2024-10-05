fb781d187fbe41ae417f74988bc614a516684be2
hnguyenne <nguyenb2105679@student.ctu.edu.vn>
2024-10-05 20:16:34 +0700

    setup express server

---
+++ backend-api/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    return res.json({
+        message: 'OK'
+    });
+});
+
+module.exports = app;
