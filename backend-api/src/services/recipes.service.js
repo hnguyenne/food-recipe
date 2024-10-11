c9ebca2dd03b6c37ae2fb97548fed5d02375ad31
Luapxana <luapisnothere23@gmail.com>
2024-10-11 09:42:07 +0700

    Add recipe services
    add recipe, get many recipes, get recipe by id, upate recipe, add or remove a tag from recipe, delete recipe

--- backend-api/src/services/recipes.service.js
+++ backend-api/src/services/recipes.service.js
@@ -3,59 +3,136 @@ const knex = require("../database/knex");
 function recipeRepository(){
     return knex('recipes')
 }
-function ingredientRepository(){
-    return knex('ingredients')
-}
-function stepRepository(){
-    return knex('steps')
+function RecipeTagRepository(){
+    return knex('recipe_tag')
 }
 
 function readRecipe(payload){
     return {
+        user_id: payload.user_id,
         tittle: payload.tittle,
         description: payload.description,
         prep_time: payload.prep_time,
         cook_time: payload.cook_time,
         servings: payload.servings,
-        image_url: payload.image_url,
+        instruction: payload.instruction,
         video_path: payload.video_path,
         recipe_create_at: payload.recipe_create_at,
-        user_id: payload.user_id,
-        ingredient_id: payload.ingredient_id,
-        ingredient_description: payload.ingredient_description,
-        step_id: payload.step_id,
-        instruction: payload.instruction,
+        note: payload.note,
+        img_url: payload.img_url,
         tag_id: payload.tag_id
     }
 }
 
 async function addRecipe(payload) {
     const recipe = readRecipe(payload);
-    try{
-        const [ recipe_id ] = await recipeRepository().insert(recipe).returning('id');
-        if (payload.ingredients) {
-            await Promise.all(payload.ingredients.map(async (ingredient) => {
-                const [ingredientId] = await knex('ingredients').insert({
-                    ingredient_name: ingredient.ingredient_name,
-                    description: ingredient.ingredient_description,
-                    recipe_id: ingredient.recipe_id
-                }).returning('ingredient_id');
-            }));
-        }
+    const [ recipe_id ] = await recipeRepository().insert(recipe);
+    return { recipe_id, ...recipe };
+}
 
-        if (payload.steps) {
-            await Promise.all(payload.steps.map(async (step, index) => {
-                await knex('steps').insert({
-                    recipe_id: recipe_id,
-                    instruction: step.instruction,
-                    step_number: index + 1, // Assuming step numbers start from 1
-                });
-            }));
+async function getRecipes(query){
+    const { name, tag, description } = query; // Get recipes by name, tag, descrition (how to cook, ingredient)
+    return contactRepository()
+            .join('recipe_tags', 'recipes.recipe_id', '=', 'recipe_tag.recipe_id')
+            .join('tags', 'tag_recipe.tag_id', '=' , 'tags.tag_id')
+            .where((builder) => {
+                if(name){
+                    buidler.where('name', 'like', '%${name}%');
+                }
+                if (tag){
+                    builder.where('tag', '=', '${tag}')
+                }
+                if(description){
+                    builder.description('desciption', 'like', '%${description}%')
+                }
+            }).select('*');
+}
+
+async function getRecipeById(id){
+    return recipeRepository()
+        .join('recipe_tags', 'recipes.recipe_id', '=', 'recipe_tag.recipe_id')
+        .join('tags', 'tag_recipe.tag_id', '=' , 'tags.tag_id')
+        .where('recipe_id', id).select('*').first();
+}
+
+async function updateRecipe(id, payload){
+    const updatedRecipe = await recipeRepository()
+                        .where('recipe_id', id)
+                        .select('*').first();
+    if (!updatedRecipe) {
+        return null;
+    }
+    const update = readRecipe(payload)
+    if (!update.img_url){
+        delete update.img_url;
+    }
+    if (!update.video_path){
+        delete update.video_path;
+    }
+    await recipeRepository()
+    .where('id', id).update(update);
+    if (
+        update.img_url && updatedRecipe.img_url &&
+        update.img_url !== updatedRecipe.img_url &&
+        updatedRecipe.img_url.startsWith('/public/uploads/images/')
+    ){
+        unlink('.${updatedRecipe.img_url}', (err) => {})
+    }
+    if (
+        update.video_path && updatedRecipe.video_path &&
+        update.video_path !== updatedRecipe.video_path &&
+        updatedRecipe.video_path.startsWith('/public/uploads/videos/')
+    ){
+        unlink('.${updatedRecipe.video_path}', (err) => {})
+    }
+    return { ...updatedRecipe, ...update }
+}
+
+async function addRecipeTag(id, tag){ 
+    const existing = await RecipeTagRepository()
+                    .where('recipe_id', id)
+                    .andWhere('tag_id', tag_id)
+                    .select('*').first()
+    if (!existing){
+        const tag = await RecipeTagRepository().insert({
+                    recipe_id: id,
+                    tag_id: tagid
+                })
+    }
+    return tag;
+}
+
+async function removeRecipeTag(id, tag){
+    const deleted = await RecipeTagRepository()
+                    .where('recipe_id', id)
+                    .andWhere('tag_id', tag).first();
+        if (!deleted){
+            return null;
         }
-        return { id: recipeId, ...recipeData };
+        await RecipeTagRepository()
+        .where('recipe_id', id)
+        .andWhere('tag_id', tag).del();
+    return deleted;
+}
+
+async function deleteRecipe(id){
+    const deleted = await recipeRepository()
+                    .where('id', id)
+                    .select('img_url')
+                    .first();
+    if (!deleted){
+        return null;
     }
-    catch (error) {
-        console.error('Error adding recipe:', error);
-        throw error;
-    }
+    await recipeRepository().where('id', id).del();
+    return deleted;
+}
+
+module.exports = {
+    addRecipe,
+    getRecipes,
+    getRecipeById,
+    updateRecipe,
+    addRecipeTag,
+    removeRecipeTag,
+    deleteRecipe
 }
