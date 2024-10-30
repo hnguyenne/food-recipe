289d3d281e3c20ae963570afce0707ca7ad714cc
hong-nguyen13 <nguyenb2105679@student.ctu.edu.vn>
2024-10-30 20:50:50 +0700

    add review service (frontend)

---
+++ frontend/src/services/reviews.service.js
@@ -0,0 +1,67 @@
+/**
+* @param {string} url
+
+* @param {RequestInit} options
+* @returns Promise<Response>
+*/
+
+async function efetch(url, options = {}) {
+    let result = {};
+    let json = {};
+    try {
+        result = await fetch(url, options);
+        json = await result.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+    if (!result.ok || json.status !== 'success') {
+        throw new Error(json.message);
+    }
+    return json.data;
+}
+
+function makeReviewsService() {
+
+    const baseUrl = '/api/v1/reviews';
+
+    async function addReviews(recipe_id, review) {
+        return efetch(`${baseUrl}/?recipe_id=${recipe_id}`, {
+            method: 'POST',
+            body: review,
+        });
+    }
+
+    async function getReviews(recipe_id) {
+        return efetch(`${baseUrl}/?recipe_id=${recipe_id}`, {
+            method: 'GET',
+        });
+    }
+
+    async function getAvgRate(recipe_id) {
+        return efetch(`${baseUrl}/avg/?recipe_id=${recipe_id}`, {
+            method: 'GET',
+        });
+    }
+
+    async function deleteReview(review_id) {
+        return efetch(`${baseUrl}/${review_id}`, {
+            method: 'DELETE',
+        });
+    }
+
+    async function updateReview(review_id, review) {
+        return efetch(`${baseUrl}/${review_id}`, {
+            method: 'PUT',
+            body: review,
+        });
+    }
+
+    return {
+        addReviews,
+        getReviews,      
+        getAvgRate,
+        deleteReview,
+        updateReview,  
+    };
+}
+export default makeReviewsService();
